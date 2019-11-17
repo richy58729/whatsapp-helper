3e8df7342f95067891f1c3f813ed38c073ce160e
R.J. Vrijhof <r@vrijhof.info>
2019-11-17 22:13:30 +0100

    First import. Already working, waiting to be refined some more.

---
+++ README.md
@@ -0,0 +1,7 @@
+# WhatsApp helper #
+
+## Web page with some JavaScript to help with using https://wa.me for sending a WhatsApp message to a telephone number ##
+
+With this, you can use [https://wa.me/](https://wa.me/) to send a WhatsApp message to a telephone number by means of a web browser, meant mainly to be used on a desktop pc, but it should also work on a smartphone. It assumes you are either using the WhatsApp desktop client or WhatsApp web in the same browser.
+
+To use this, you need a web server. Which software you use for the web server, is irrelevant, as it is just a static HTML page with some JavaScript and CSS.
---
+++ nginx/www/index.js
@@ -0,0 +1,124 @@
+'use strict';
+
+if (location.protocol !== 'https:' & location.host === 'whatsapp-helper.appspot.com') {
+  location.href = 'https://whatsapp-helper.appspot.com/';
+}
+
+var countrycode, currentElement;
+
+setTimeout(
+  function() {
+    document.getElementById('countrycode').focus();
+    document.onkeyup = function(e) {
+      if (document.activeElement.id !== 'countrycode') {
+        if (e.keyCode === 13) {
+          doStuff();
+        }
+      }
+    }
+  },
+  50
+);
+
+function doFilter(e) {
+  var div, filter, i, input, txtValue;
+  e = e || window.event;
+  input = document.getElementById('countrycode');
+  refreshList();
+  if (e.keyCode === 27) {
+    input.value = '';
+    filter = input.value.toUpperCase();
+    refreshList();
+    return;
+  } else if (e.keyCode === 40) {
+    if (countrycode === null) {
+      currentElement = document.querySelector('.dropdown-content div div');
+      if (currentElement.style.display !== '') {
+        while (currentElement.nextElementSibling !== null) {
+          currentElement = currentElement.nextElementSibling;
+          if (currentElement.style.display === '') {
+            break;
+          }
+        }
+      }
+    } else if (currentElement.nextElementSibling !== null) {
+      currentElement.style.backgroundColor = '#f1f1f1';
+      currentElement.parentNode.scrollBy(0, 15);
+      while (currentElement.nextElementSibling !== null) {
+        currentElement = currentElement.nextElementSibling;
+        if (currentElement.style.display === '') {
+          break;
+        }
+      }
+    }
+  } else if (e.keyCode === 38) {
+    if (countrycode === null) {
+      currentElement = document.querySelector('.dropdown-content div div');
+      if (currentElement.style.display !== '') {
+        while (currentElement.previousElementSibling !== null) {
+          currentElement = currentElement.previousElementSibling;
+          if (currentElement.style.display === '') {
+            break;
+          }
+        }
+      }
+    } else if (currentElement.previousElementSibling !== null) {
+      currentElement.style.backgroundColor = '#f1f1f1';
+      currentElement.parentNode.scrollBy(0, -15);
+      while (currentElement.previousElementSibling !== null) {
+        currentElement = currentElement.previousElementSibling;
+        if (currentElement.style.display === '') {
+          break;
+        }
+      }
+    }
+  } else {
+    countrycode = null;
+    if (currentElement !== undefined && currentElement !== null) {
+      currentElement.style.backgroundColor = "#f1f1f1";
+    }
+    return;
+  }
+  countrycode = currentElement.getAttributeNode('data-countrycode').value;
+  currentElement.style.backgroundColor = '#d1d1d1';
+} // function doFilter
+
+function doSelect(element) {
+  var div, i;
+  countrycode = element.getAttributeNode('data-countrycode').value;
+  div = document.querySelectorAll('.dropdown-content div div');
+  for (i = 0; i < div.length; i++) {
+    div[i].style.backgroundColor = '#f1f1f1';
+  }
+  element.style.backgroundColor = '#d1d1d1';
+} // function doSelect
+
+function doStuff() {
+  var myWindow;
+  myWindow = window.open(
+    'https://wa.me/' + countrycode + document.querySelector('input#prefix').value +
+    document.querySelector('input#subscriber').value,
+    '_blank'
+  );
+  setTimeout(
+    function() {
+      myWindow.close();
+    },
+    1500
+  );
+} // function doStuff
+
+function refreshList() {
+  var div, filter, i, input, txtValue;
+  input = document.getElementById('countrycode');
+  filter = input.value.toUpperCase();
+  div = document.querySelectorAll('.dropdown-content div div');
+  for (i = 0; i < div.length; i++) {
+    txtValue = div[i].textContent || div[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      div[i].style.display = '';
+    } else {
+      div[i].style.display = 'none';
+    }
+  }
+} // function refreshList
